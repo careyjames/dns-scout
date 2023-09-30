5e889e400df75da8257579ad8c50dbeab94a7281
Carey Balboa <52982329+careyjames@users.noreply.github.com>
2023-09-30 16:39:04 -0500

    Update XtraDNS.py

--- XtraDNS.py
+++ XtraDNS.py
@@ -1,61 +1,116 @@
+# Version: v4.9
+
 import subprocess
+import sys
+from prompt_toolkit import prompt
+from prompt_toolkit.history import InMemoryHistory
+from prompt_toolkit.clipboard.in_memory import InMemoryClipboard
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.formatted_text import FormattedText
 
-# Define a function to get the Registrar and Name Server information from WHOIS data
+# Ensure Python dependencies
+def ensure_python_dependencies():
+    try:
+        from prompt_toolkit import prompt
+        import colorama
+    except ImportError:
+        print("Some Python modules are missing. Attempting to install necessary modules...")
+        subprocess.run([sys.executable, "-m", "pip", "install", "prompt_toolkit", "colorama"])
+
+# Check Python dependencies
+ensure_python_dependencies()
+
+from colorama import Fore, init
+init(autoreset=True)  # Automatically reset terminal color to default after print statements
+
+# Define the pasting keybindings
+kb = KeyBindings()
+
+@kb.add(Keys.BracketedPaste)
+def _(event):
+    event.current_buffer.insert_text(event.data, fire_event=False)
+
+# Setup command history
+history = InMemoryHistory()
+
 def get_info(start_line, lines):
+    registrar = None
+    name_servers = []
+    i = start_line  # Initialize the index
     for i in range(start_line, len(lines)):
-        # If the line starts with 'Name Server:', print the Name Servers
-        if lines[i].startswith('Name Server:'):
-            print('Name Servers:', lines[i].split(': ')[1])
-        # If the line starts with 'Registrar:', print the Registrar
-        elif lines[i].startswith('Registrar:'):
-            print('Registrar:', lines[i].split(': ')[1])
-    # Return the current line number
-    return i
+        line = lines[i]
+        if "Registrar:" in line and registrar is None:
+            registrar = line.split("Registrar:")[1].strip()
+            print(Fore.GREEN + "Registrar:", registrar)
+        if "Name Server:" in line:
+            name_servers.append(line.split("Name Server:")[1].strip())
+        if line.strip() == "" and name_servers:
+            break
 
-# Define a function to print DNS record information
-def print_dns_records(records):
-    # Loop through each record
-    for record in records:
-        # If the record is a mail exchanger (MX) record, print it
-        if 'mail exchanger' in record:
-            print('MX Records:', record.split('= ')[1])
-        # If the record is a text (TXT) record, print it
-        elif 'text =' in record:
-            print('TXT Records:', record.split('= ')[1])
-        # If the record is a DMARC record, print it
-        elif '_dmarc' in record:
-            print('DMARC Record:', record.split('= ')[1])
+    if name_servers:
+        print(Fore.YELLOW + "Name Servers:", ", ".join(name_servers))
+        return i, True
+    return i, False
 
-# Ask the user to input a domain
-domain = input("Enter domain: ")
+def print_dns_records(mx_records, txt_records, dmarc_record):
+    # MX Records
+    print(Fore.MAGENTA + "\nMX Records:")
+    for line in mx_records:
+        if "mail exchanger" in line:
+            print("  ", line.split("=")[1].strip())
 
-# Run the WHOIS command on the domain and capture the output
-result = subprocess.run(["whois", domain], capture_output=True, text=True)
+    # TXT Records
+    print(Fore.MAGENTA + "\nTXT Records:")
+    for line in txt_records:
+        if "text =" in line:
+            txt = line.split("text =")[1].strip()
+            print("  ", txt)
 
-# Split the output into lines
-lines = result.stdout.splitlines()
-start_line = 0
+    # DMARC Records
+    print(Fore.MAGENTA + "\nDMARC Records:")
+    for line in dmarc_record:
+        if "text =" in line:
+            txt = line.split("text =")[1].strip()
+            print("  ", txt)
 
-# Keep getting info until we've gone through all the lines
+# Main loop
 while True:
-    start_line = get_info(start_line, lines) + 1
-    if start_line >= len(lines):
+    print('-' * 80)  # Line separator
+    domain = prompt(
+        FormattedText([('cyan', 'Enter domain (or \'exit\' to quit): ')]),
+        history=history,  # Add the history argument here
+        key_bindings=kb,
+        clipboard=InMemoryClipboard()
+    ).strip()
+
+    if domain.lower() in ['exit', 'q', 'quit']:
         break
 
-# Run the NSLOOKUP command on the domain for MX records and capture the output, then split it into lines
-mx_records = subprocess.check_output(["nslookup", "-type=mx", domain]).decode("utf-8").splitlines()
+    result = subprocess.run(["whois", domain], capture_output=True, text=True)
+    lines = result.stdout.splitlines()
+    start_line = 0
+    registrar_printed = False
 
-# Run the NSLOOKUP command on the domain for TXT records and capture the output, then split it into lines
-txt_records = subprocess.check_output(["nslookup", "-type=txt", domain]).decode("utf-8").splitlines()
+    while not registrar_printed:
+        start_line, registrar_printed = get_info(start_line, lines)
+        start_line += 1
+        if start_line >= len(lines):
+            break
 
-# Try to run the NSLOOKUP command on the domain for DMARC records and capture the output, then split it into lines
-try:
-    dmarc_record = subprocess.check_output(["nslookup", "-type=txt", "_dmarc."+domain]).decode("utf-8").splitlines()
-except subprocess.CalledProcessError:
-    # If the command fails (which might happen if there is no DMARC record), make dmarc_record an empty list
-    dmarc_record = []
+    try:
+        mx_records = subprocess.check_output(["nslookup", "-type=mx", domain]).decode("utf-8").splitlines()
+    except subprocess.CalledProcessError:
+        mx_records = []
 
-# Print the DNS records
-print_dns_records(mx_records)
-print_dns_records(txt_records)
-print_dns_records(dmarc_record)
+    try:
+        txt_records = subprocess.check_output(["nslookup", "-type=txt", domain]).decode("utf-8").splitlines()
+    except subprocess.CalledProcessError:
+        txt_records = []
+
+    try:
+        dmarc_record = subprocess.check_output(["nslookup", "-type=txt", "_dmarc."+domain]).decode("utf-8").splitlines()
+    except subprocess.CalledProcessError:
+        dmarc_record = []
+
+    print_dns_records(mx_records, txt_records, dmarc_record)
