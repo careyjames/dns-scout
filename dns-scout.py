03299d14a8bb38428e211b2ce7492ea67858910b
Carey Balboa <52982329+careyjames@users.noreply.github.com>
2023-10-05 21:12:44 -0500

    Update dns-scout.py v5.5
    Update dns-scout.py v5.5

--- dns-scout.py
+++ dns-scout.py
@@ -1,132 +1,131 @@
-# DNS Scout - Version: v5.0
+# DNS Scout - Version: v5.5
 
-import subprocess
-import sys
-from prompt_toolkit import prompt
-from prompt_toolkit.history import InMemoryHistory
-from prompt_toolkit.clipboard.in_memory import InMemoryClipboard
-from prompt_toolkit.key_binding import KeyBindings
-from prompt_toolkit.keys import Keys
-from prompt_toolkit.formatted_text import FormattedText
+import dns.resolver
+from prompt_toolkit import PromptSession, HTML
+import whois
 
-# Ensure Python dependencies
-def ensure_python_dependencies():
+MARGIN = '    '
+
+def get_registrar(domain):
     try:
-        from prompt_toolkit import prompt
-        import colorama
-    except ImportError:
-        print("Some Python modules are missing. Attempting to install necessary modules...")
-        subprocess.run([sys.executable, "-m", "pip", "install", "prompt_toolkit", "colorama"])
+        w = whois.whois(domain)
+        if w.registrar:
+            return w.registrar
+        else:
+            return "Unknown"
+    except:
+        return "Unknown"
 
-# Check Python dependencies
-ensure_python_dependencies()
-
-from colorama import Fore, init
-init(autoreset=True)  # Automatically reset terminal color to default after print statements
-
-# Define the pasting keybindings
-kb = KeyBindings()
-
-@kb.add(Keys.BracketedPaste)
-def _(event):
-    event.current_buffer.insert_text(event.data, fire_event=False)
-
-# Setup command history
-history = InMemoryHistory()
-
-def get_info(start_line, lines):
-    registrar = None
-    name_servers = []
-    i = start_line
-    for i in range(start_line, len(lines)):
-        line = lines[i]
-        if "Registrar:" in line and registrar is None:
-            registrar = line.split("Registrar:")[1].strip()
-            print(Fore.GREEN + "Registrar:", registrar)
-        if "Name Server:" in line:
-            name_servers.append(line.split("Name Server:")[1].strip())
-        if line.strip() == "" and name_servers:
-            break
-
-    if name_servers:
-        print(Fore.YELLOW + "Name Servers:", ", ".join(name_servers))
-        return i, True
-    return i, False
-
-def print_dns_records(mx_records, txt_records, dmarc_record, ptr_records):
-    # MX Records
-    print(Fore.MAGENTA + "\nMX Records:")
-    for line in mx_records:
-        if "mail exchanger" in line:
-            print("  ", line.split("=")[1].strip())
-
-    # TXT Records
-    print(Fore.MAGENTA + "\nTXT Records:")
-    for line in txt_records:
-        if "text =" in line:
-            txt = line.split("text =")[1].strip()
-            print("  ", txt)
-
-    # DMARC Records
-    print(Fore.MAGENTA + "\nDMARC Records:")
-    for line in dmarc_record:
-        if "text =" in line:
-            txt = line.split("text =")[1].strip()
-            print("  ", txt)
-
-    # PTR Records
-    print(Fore.MAGENTA + "\nPTR Records:")
-    ptr_found = False
-    for line in ptr_records:
-        if "name =" in line:
-            ptr = line.split("name =")[1].strip()
-            print("  ", ptr)
-            ptr_found = True
-    if not ptr_found:
-        print("  None found")
-
-# Main loop
-while True:
-    print('-' * 80)  # Line separator
-    domain = prompt(
-        FormattedText([('cyan', 'Enter domain (or \'exit\' to quit): ')]),
-        history=history,
-        key_bindings=kb,
-        clipboard=InMemoryClipboard()
-    ).strip()
-
-    if domain.lower() in ['exit', 'q', 'quit']:
-        break
-
-    result = subprocess.run(["whois", domain], capture_output=True, text=True)
-    lines = result.stdout.splitlines()
-    start_line = 0
-    registrar_printed = False
-
-    while not registrar_printed:
-        start_line, registrar_printed = get_info(start_line, lines)
-        start_line += 1
-        if start_line >= len(lines):
-            break
+def get_info(domain):
+    try:
+        ns = dns.resolver.resolve(domain, 'NS')
+        name_servers = [item.to_text() for item in ns]
+    except:
+        name_servers = None
 
     try:
-        mx_records = subprocess.check_output(["nslookup", "-type=mx", domain]).decode("utf-8").splitlines()
-    except subprocess.CalledProcessError:
-        mx_records = []
+        mx = dns.resolver.resolve(domain, 'MX')
+        mx_records = [item.exchange.to_text() for item in mx]
+    except:
+        mx_records = None
 
     try:
-        txt_records = subprocess.check_output(["nslookup", "-type=txt", domain]).decode("utf-8").splitlines()
-    except subprocess.CalledProcessError:
+        txt = dns.resolver.resolve(domain, 'TXT')
+        txt_records = [item.strings[0].decode("utf-8") for item in txt]
+    except:
         txt_records = []
 
     try:
-        dmarc_record = subprocess.check_output(["nslookup", "-type=txt", "_dmarc."+domain]).decode("utf-8").splitlines()
-    except subprocess.CalledProcessError:
-        dmarc_record = []
+        dmarc = dns.resolver.resolve(f'_dmarc.{domain}', 'TXT')
+        dmarc_record = dmarc[0].strings[0].decode("utf-8")
+        if "v=DMARC1;" not in dmarc_record:
+            dmarc_record = None
+    except:
+        dmarc_record = None
 
     try:
-        ptr_records = subprocess.check_output(["nslookup", "-q=ptr", domain]).decode("utf-8").splitlines()
-    except subprocess.CalledProcessError:
-        ptr_records = []
+        ptr = dns.resolver.resolve(domain, 'PTR')
+        ptr_records = [item.to_text() for item in ptr]
+    except:
+        ptr_records = None
 
-    print_dns_records(mx_records, txt_records, dmarc_record, ptr_records)
+    return name_servers, mx_records, txt_records, dmarc_record, ptr_records
+
+def print_dns_records(registrar, name_servers, mx_records, txt_records, dmarc_record, ptr_records):
+    print(f"{MARGIN}\033[94m{'-' * 76}\033[0m")
+    if registrar == 'Unknown':
+        print(f"{MARGIN}\033[95mRegistrar:\033[0m \033[93mUnknown or Classified\033[0m")
+    else:
+        print(f"{MARGIN}\033[95mRegistrar:\033[0m {registrar}")
+
+
+    # Name Servers
+    print(f"\033[95m{MARGIN}Name Servers:\033[0m")
+    if name_servers:
+        for server in name_servers:
+            print(f"{MARGIN}   {server}")
+    else:
+        print(f"{MARGIN}   None")
+
+    # MX Records
+    print(f"\033[95m{MARGIN}MX Records:\033[0m")
+    if mx_records:
+        for record in mx_records:
+            print(f"{MARGIN}   {record}")
+    else:
+        print(f"{MARGIN}\033[91m   No MX record, No email!\033[0m")
+
+    # DMARC Records
+    print(f"\033[95m{MARGIN}DMARC Records:\033[0m")
+    if dmarc_record:
+        if "p=quarantine;" in dmarc_record:
+            dmarc_record = dmarc_record.replace("p=quarantine;", "\033[93mp=quarantine;\033[0m")
+        if "sp=quarantine;" in dmarc_record:
+            dmarc_record = dmarc_record.replace("sp=quarantine;", "\033[93msp=quarantine;\033[0m")
+        if "p=none;" in dmarc_record:
+            dmarc_record = dmarc_record.replace("p=none;", "\033[91mp=none;\033[0m")
+        
+        for i in range(1, 100):  # Checking pct values from 1 to 99
+            if f"pct={i};" in dmarc_record:
+                dmarc_record = dmarc_record.replace(f"pct={i};", f"\033[91mpct={i};\033[0m")
+
+        print(f"{MARGIN}   {dmarc_record}")
+    else:
+        print(f"{MARGIN}\033[91m   None\033[0m")
+
+    # TXT Records
+    print(f"\033[95m{MARGIN}TXT Records:\033[0m")
+    for record in txt_records:
+        if "v=spf1" in record or "v=spf2.0/pra" in record or "~all" in record or "-all" in record:
+            if "vspf" in record:
+                record = record.replace("vspf", "\033[91mvspf\033[0m")
+            if "~all" in record:
+                print(f"{MARGIN}\033[32m{record.split('~all')[0]}\033[92m~all\033[0m")
+            elif "-all" in record:
+                print(f"{MARGIN}\033[32m{record.split('-all')[0]}\033[93m-all\033[0m")
+            else:
+                print(f"{MARGIN}\033[32m{record}\033[0m")
+        else:
+            print(f"{MARGIN}\033[32m{record}\033[0m")
+
+    # PTR Records
+    print(f"\033[95m{MARGIN}PTR Records:\033[0m")
+    if ptr_records:
+        for record in ptr_records:
+            print(f"{MARGIN}   {record}")
+    else:
+        print(f"{MARGIN}\033[93m   None\033[0m")
+
+    print(f"{MARGIN}\033[94m{'-' * 76}\033[0m")
+
+session = PromptSession()
+
+while True:
+    domain = session.prompt(HTML(f'{MARGIN}<ansibrightblue>Enter domain (or \'exit\' to quit): </ansibrightblue>'))
+
+    if domain.lower() == 'exit':
+        break
+
+    registrar = get_registrar(domain) 
+    name_servers, mx_records, txt_records, dmarc_record, ptr_records = get_info(domain)
+    print_dns_records(registrar, name_servers, mx_records, txt_records, dmarc_record, ptr_records)
