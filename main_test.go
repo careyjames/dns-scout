8fd72daef297629167688328dde19c7412d351c1
Carey Balboa <carey.balboa@machelpnashville.com>
2023-10-14 12:09:07 -0500

    add test case

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -26,4 +29,67 @@ func TestGetRegistrar(t *testing.T) {
 		t.Errorf("Expected registrar: %s, but got: %s", expected, result)
 	}
 	// You can add more test cases here to cover additional scenarios.
+}
+
+// Mock HTTP server for testing
+func mockServer(statusCode int, responseJSON string) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(statusCode)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(responseJSON))
+	}))
+}
+
+func TestGetASNInfo(t *testing.T) {
+	// Test case 1: Successful response
+	// Test case 1: Successful response
+	t.Run("Success", func(t *testing.T) {
+		// Mock server with a successful response
+		mock := mockServer(http.StatusOK, `{
+			"asn": {
+				"asn": "AS12345",
+				"name": "Example ASN"
+			},
+			"ip": "8.8.8.8",
+			"domain": "example.com",
+			"hostname": "host.example.com",
+			"city": "City",
+			"region": "Region",
+			"country": "US",
+			"loc": "0.0,0.0",
+			"org": "Example Org",
+			"postal": "12345",
+			"timezone": "UTC",
+			"readme": "Test data"
+		}`)
+		defer mock.Close()
+
+		ip := "8.8.8.8"
+		apiToken := "your-api-token"
+
+		info, err := getASNInfo(ip, apiToken)
+		if err != nil {
+			t.Errorf("Expected no error, got %v", err)
+		}
+
+		if info == nil {
+			t.Errorf("Expected non-nil IPInfoResponse, got nil")
+		}
+
+		expected := &IPInfoResponse{
+			ASN: map[string]interface{}{"asn": "AS12345", "name": "Example ASN"},
+			IP:  "8.8.8.8", Domain: "example.com", Hostname: "host.example.com",
+			City: "City", Region: "Region", Country: "US", Loc: "0.0,0.0",
+			Org: "Example Org", Postal: "12345", Timezone: "UTC", Readme: "Test data",
+		}
+		if compareIPInfoResponse(info, expected) {
+			t.Errorf("Expected %+v, got %+v", expected, info)
+		}
+	})
+}
+
+func compareIPInfoResponse(a, b *IPInfoResponse) bool {
+	aJSON, _ := json.Marshal(a)
+	bJSON, _ := json.Marshal(b)
+	return string(aJSON) == string(bJSON)
 }
