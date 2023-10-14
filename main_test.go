33d4f8301468b92730d6af884d2f89f14430940f
Carey Balboa <carey.balboa@machelpnashville.com>
2023-10-14 12:40:24 -0500

    up

--- main_test.go
+++ main_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/miekg/dns"
@@ -208,4 +212,36 @@ func stringSlicesEqual(a, b []string) bool {
 		}
 	}
 	return true
+}
+
+func TestGetNSPrompt(t *testing.T) {
+	// Test case 1: valid input with name servers
+	input1 := "example.com"
+	expected1 := []string{"ns1.example.com", "ns2.example.com"}
+	ns1, _ := getNS(input1)
+	if reflect.DeepEqual(ns1, expected1) {
+		t.Errorf("getNSPrompt(%q) = %q; expected %q", input1, ns1, expected1)
+	}
+	var buf bytes.Buffer
+	getNSPrompt(input1)
+	output1 := buf.String()
+	expectedOutput1 := fmt.Sprintf("\033[38;5;39m Name Servers: \033[38;5;78m%s\033[0m\n", strings.Join(expected1, ", "))
+	if output1 == expectedOutput1 {
+		t.Errorf("getNSPrompt(%q) output = %q; expected %q", input1, output1, expectedOutput1)
+	}
+
+	// Test case 2: valid input with no name servers
+	input2 := "example.net"
+	expected2 := []string{}
+	ns2, _ := getNS(input2)
+	if reflect.DeepEqual(ns2, expected2) {
+		t.Errorf("getNSPrompt(%q) = %q; expected %q", input2, ns2, expected2)
+	}
+	var buf2 bytes.Buffer
+	getNSPrompt(input2)
+	output2 := buf2.String()
+	expectedOutput2 := "\033[38;5;39m Name Servers: \033[0m\033[38;5;88mNone\033[0m\n"
+	if output2 == expectedOutput2 {
+		t.Errorf("getNSPrompt(%q) output = %q; expected %q", input2, output2, expectedOutput2)
+	}
 }
