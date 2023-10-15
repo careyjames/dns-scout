cab65c8b2dfee86c5eb6740a5568c203f38cb27c
tushargupta0694 <tushar.gupta@finfloh.com>
2023-10-15 21:55:31 +0530

    break code in multiple segment

--- build.sh
+++ build.sh
@@ -5,18 +5,18 @@
 
 # Step 1: Compile the Go code
 echo "Compiling Go code..."
-go build -v -o ./bin/dns-scout main.go
+go build -v -o ./bin/dns-scout
 
-GOOS=darwin GOARCH=amd64 go build -v -o ./dns-scout-macos-amd64-intel-v5.8/dns-scout main.go
+GOOS=darwin GOARCH=amd64 go build -v -o ./dns-scout-macos-amd64-intel-v5.8/dns-scout
 # produces a binary for macOS running on Intel x86_64 architecture (Intel Macs). It does not produce a binary for the newer Apple Silicon Macs, which use the ARM64 architecture.
 
-GOOS=darwin GOARCH=arm64 go build -v -o ./dns-scout-macos-arm64-silicon-v5.8/dns-scout main.go
+GOOS=darwin GOARCH=arm64 go build -v -o ./dns-scout-macos-arm64-silicon-v5.8/dns-scout
 # This will produce a binary (dns-scout-macos-arm64) that runs on macOS systems with ARM64 architecture (Apple Silicon).
 
-GOOS=linux GOARCH=amd64 go build -v -o ./dns-scout-linux-amd64-ubuntu-kali-v5.8/dns-scout main.go
+GOOS=linux GOARCH=amd64 go build -v -o ./dns-scout-linux-amd64-ubuntu-kali-v5.8/dns-scout
 # This will generate a binary (dns-scout-linux-amd64) that is suitable for most Kali and Ubuntu installations on AMD64/x86_64 hardware.
 
-GOOS=linux GOARCH=386 go build -v -o ./dns-scout-linux-386-v5.8/dns-scout main.go
+GOOS=linux GOARCH=386 go build -v -o ./dns-scout-linux-386-v5.8/dns-scout
 # If you want to support older 32-bit machines or other architectures, you'll need to specify different GOARCH values. For example, for 32-bit x86:
 
 tar czvf dns-scout-macos-amd64-intel-v5.8.tar.gz --transform 's,^./dns-scout-macos-amd64-intel-v5.8/dns-scout,dns-scout,' ./dns-scout-macos-amd64-intel-v5.8/dns-scout ./README.md ./setup-api-token.sh
