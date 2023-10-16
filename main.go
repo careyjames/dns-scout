27444257f3e752dbbf934978465b91a94d5bb386
tushargupta0694 <tushar.gupta@finfloh.com>
2023-10-16 21:19:28 +0530

    correct-dmarc

--- main.go
+++ main.go
@@ -73,11 +73,16 @@ func getTXT(domain string) ([]string, error) {
 // getDMARC fetches the DMARC record for a given domain.
 func getDMARC(domain string) (string, error) {
 	txtRecords, err := getTXT("_dmarc." + domain)
+	if len(txtRecords) <= 0 {
+		txtRecords, _ = GetDMARCRecordNSLookup(domain)
+	}
 	if err != nil {
 		return "", err
 	}
 	for _, record := range txtRecords {
 		if len(record) > 8 && record[:8] == "v=DMARC1" {
+			return record, nil
+		} else if record[:6] == "vDMARC" {
 			return record, nil
 		}
 	}
@@ -279,12 +284,23 @@ func getTXTPrompt(input string) {
 	}
 }
 
+func isValidDMARC(record string) bool {
+	if len(record) > 8 && record[:8] == "v=DMARC1" {
+		return true
+	}
+	return false
+}
+
 func getDMARCPrompt(input string) {
 	dmarc, _ := getDMARC(input)
 	if dmarc != "" {
-		formattedDMARC := formatLongText(dmarc, 80, " ")
-		fmt.Printf("\033[38;5;39m DMARC Record:\033[0m\n")
-		fmt.Printf("\033[38;5;78m %s\033[0m\n", formattedDMARC)
+		if isValidDMARC(dmarc) {
+			formattedDMARC := formatLongText(dmarc, 80, " ")
+			fmt.Printf("\033[38;5;39m DMARC Record:\033[0m\n")
+			fmt.Printf("\033[38;5;78m %s\033[0m\n", formattedDMARC)
+		} else {
+			fmt.Printf("\033[38;5;39m DMARC Record: \033[0m\033[38;5;88m%s\033[0m\n", dmarc[8:])
+		}
 	} else {
 		fmt.Printf("\033[38;5;39m DMARC Record: \033[0m\033[38;5;88mNone\033[0m\n")
 	}
--- ns_lookup.go
+++ ns_lookup.go
@@ -17,3 +17,17 @@ func GetTXTRecordNSLookup(domain string) ([]string, error) {
 	}
 	return records, nil
 }
+
+// GetTXTRecordNSLookup fetch txt ns lookup
+func GetDMARCRecordNSLookup(domain string) ([]string, error) {
+	dns_txt_records, err := net.LookupTXT("_dmarc." + domain)
+	records := []string{}
+	if err == nil && len(dns_txt_records) > 0 {
+		for _, record := range dns_txt_records {
+			records = append(records, record)
+		}
+	} else {
+		return records, err
+	}
+	return records, nil
+}
