4eebb05ee01c474eb5fdcc30bd17c6cbbab2d7f6
Carey Balboa <carey.balboa@machelpnashville.com>
2023-10-19 10:50:13 -0500

    v6.0 baby!

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Package the DNS-Scout directory into a tarball
-tar --exclude='.git' --exclude='.gitignore' --exclude='.vscode' --exclude='.tmp-history' -czvf "DNS-Scout-v5.9.tar.gz" ./bin/
+tar --exclude='.git' --exclude='.gitignore' --exclude='.vscode' --exclude='.tmp-history' -czvf "DNS-Scout-v6.0.tar.gz" ./bin/
