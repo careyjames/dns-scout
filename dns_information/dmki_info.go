5384553f6a00b65537b0badbe431215f4b2a26b6
tushargupta0694 <tushar.gupta@finfloh.com>
2023-10-19 20:35:29 +0530

    dkim records

--- dns_information/dmki_info.go
+++ dns_information/dmki_info.go
@@ -22,12 +22,8 @@ var (
 )
 
 // getDMKI fetches the DMARC record for a given domain.
-func getDKIM(domain string) (string, error) {
-	var txtRecords []string
-	for _, selector := range DKIMSelectors {
-		txtRecord, _ := GetTXTRecordNSLookup(selector + "." + domain)
-		txtRecords = append(txtRecords, txtRecord...)
-	}
+func getDKIM(domain string, selector string) (string, error) {
+	txtRecords, _ := GetTXTRecordNSLookup(selector + "." + domain)
 	for _, record := range txtRecords {
 		if hasDKIMRecord(record) {
 			return record, nil
@@ -38,16 +34,28 @@ func getDKIM(domain string) (string, error) {
 
 // GetDKIMPrompt fetches the DKIM record for a given domain.
 func GetDKIMPrompt(input string) {
-	dkmi, _ := getDKIM(input)
-	if dkmi != "" {
-		if isValidDKIM(dkmi) {
-			formattedDMARC := formatLongText(dkmi, 80, " ")
-			fmt.Printf(color.Blue(" DKIM Record: ") + color.Green(formattedDMARC) + constants.Newline)
+	flag := false
+	dkimPrompt := ""
+	for _, selector := range DKIMSelectors {
+		dkim, _ := getDKIM(input, selector)
+		if dkim != "" {
+			flag = true
+			if isValidDKIM(dkim) {
+				formattedDMARC := formatLongText(dkim, 80, " ")
+				dkimPrompt += color.Green(selector+".") + color.Green(formattedDMARC) + constants.Newline
+			} else {
+				dkimPrompt += color.Green(selector+".") + color.Red(dkim[7:]) + constants.Newline
+			}
 		} else {
-			fmt.Printf(color.Blue(" DKIM Record: ") + color.Green(dkmi[7:]) + constants.Newline)
+			if !flag {
+				dkimPrompt = color.Red("None") + constants.Newline
+			}
 		}
+	}
+	if flag {
+		fmt.Printf(color.Blue(" DKIM Record: ") + dkimPrompt)
 	} else {
-		fmt.Printf(color.Blue(" DKIM Record: ") + color.Red("None") + constants.Newline)
+		fmt.Printf(color.Blue(" DKIM Record: ") + dkimPrompt)
 	}
 }
 
