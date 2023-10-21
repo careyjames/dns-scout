15293c47a3228430bdb0832eaf9e2da5fecb58d8
tushargupta0694 <tushar.behror@gmail.com>
2023-10-21 19:54:08 +0530

    test-cases

--- dns_information/dmki_info.go
+++ dns_information/dmki_info.go
@@ -36,7 +36,6 @@ func getDKIM(domain string, selector string) (string, error) {
 func GetDKIMPrompt(input string) {
 	flag := false
 	dkimPrompt := ""
-<<<<<<< Updated upstream
 	for index, selector := range DKIMSelectors {
 		dkim, _ := getDKIM(input, selector)
 		if dkim != "" {
@@ -51,15 +50,6 @@ func GetDKIMPrompt(input string) {
 				} else {
 					dkimPrompt += color.Green(selector+".") + color.Green(formattedDMARC) + constants.Newline
 				}
-=======
-	for _, selector := range DKIMSelectors {
-		dkim, _ := getDKIM(input, selector)
-		if dkim != "" {
-			flag = true
-			if isValidDKIM(dkim) {
-				formattedDMARC := formatLongText(dkim, 80, " ")
-				dkimPrompt += color.Green(selector+".") + color.Green(formattedDMARC) + constants.Newline
->>>>>>> Stashed changes
 			} else {
 				dkimPrompt += color.Green(selector+".") + color.Red(dkim[7:]) + constants.Newline
 			}
@@ -70,15 +60,9 @@ func GetDKIMPrompt(input string) {
 		}
 	}
 	if flag {
-<<<<<<< Updated upstream
 		fmt.Printf(color.Blue(" DKIM Records: ") + dkimPrompt)
 	} else {
 		fmt.Printf(color.Blue(" DKIM Records: ") + dkimPrompt)
-=======
-		fmt.Printf(color.Blue(" DKIM Record: ") + dkimPrompt)
-	} else {
-		fmt.Printf(color.Blue(" DKIM Record: ") + dkimPrompt)
->>>>>>> Stashed changes
 	}
 }
 
