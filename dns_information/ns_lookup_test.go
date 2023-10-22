29da74d61d07c686ca22d8a2dfffd89f26ba51d1
tushargupta0694 <tushar.behror@gmail.com>
2023-10-22 09:10:23 +0530

    add test cases

--- dns_information/ns_lookup_test.go
+++ dns_information/ns_lookup_test.go
@@ -1,1 +1,48 @@
 package dnsinformation
+
+import (
+	"testing"
+)
+
+func TestGetTXTRecordNSLookup(t *testing.T) {
+	t.Run("Valid TXT Records", func(t *testing.T) {
+		// Mock the net.LookupTXT function to return TXT records.
+		domain := "google.com"
+		txtRecords, err := GetTXTRecordNSLookup(domain)
+
+		if err != nil {
+			t.Errorf("Expected no error, but got an error: %v", err)
+		}
+
+		expectedRecords := []string{
+			"TXT Record 1",
+			"TXT Record 2",
+		}
+
+		if len(txtRecords) <= 0 {
+			t.Errorf("Expected TXT records %v, but got %v", expectedRecords, txtRecords)
+		}
+	})
+
+	t.Run("No TXT Records", func(t *testing.T) {
+		// Mock the net.LookupTXT function to return an empty result.
+		domain := "nodata.com"
+		txtRecords, _ := GetTXTRecordNSLookup(domain)
+
+		if len(txtRecords) > 0 {
+			t.Error("Expected no TXT records, but got some records")
+		}
+	})
+
+	t.Run("Non-Existent Domain", func(t *testing.T) {
+		// Mock the net.LookupTXT function to return an error for a non-existent domain.
+		domain := ".com"
+		_, err := GetTXTRecordNSLookup(domain)
+
+		if err == nil {
+			t.Error("Expected an error for a non-existent domain, but got none")
+		}
+	})
+
+	// Add more test cases as needed.
+}
