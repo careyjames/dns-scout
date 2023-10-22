46b2cb74155890fc04aba7678b7199182ea939fd
Carey Balboa <carey.balboa@machelpnashville.com>
2023-10-22 14:43:07 -0500

    update

--- color/color.go
+++ color/color.go
@@ -4,12 +4,12 @@ import "fmt"
 
 // Yellow color
 func Yellow(text string) string {
-	return fmt.Sprintf("\033[38;5;222m%s\033[0m", text)
+	return fmt.Sprintf("\033[38;5;3m%s\033[0m", text)
 }
 
 // Green color
 func Green(text string) string {
-	return fmt.Sprintf("\033[38;5;78m%s\033[0m", text)
+	return fmt.Sprintf("\033[38;5;2m%s\033[0m", text)
 }
 
 // Blue color
@@ -19,5 +19,5 @@ func Blue(text string) string {
 
 // Red color
 func Red(text string) string {
-	return fmt.Sprintf("\033[38;5;88m%s\033[0m", text)
+	return fmt.Sprintf("\033[38;5;1m%s\033[0m", text)
 }
