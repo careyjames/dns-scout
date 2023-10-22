6f4962f1903b06b61b2bb115d7fd7e00705cbedb
tushargupta0694 <tushar.behror@gmail.com>
2023-10-22 09:51:16 +0530

    add test cases

--- dns_information/spf_info_test.go
+++ dns_information/spf_info_test.go
@@ -40,6 +40,53 @@ func TestIsValidSPF(t *testing.T) {
 			t.Error("Expected an invalid SPF record, but got a valid result")
 		}
 	})
+}
+
+func TestHasSPFRecord(t *testing.T) {
+	t.Run("Valid SPF Record (Starts with 'v=spf1')", func(t *testing.T) {
+		record := "v=spf1 include:_spf.example.com ~all"
+		hasSPF := HasSPFRecord(record)
+
+		if !hasSPF {
+			t.Error("Expected a valid SPF record, but got a false result")
+		}
+	})
+
+	t.Run("Valid SPF Record (Contains 'spf')", func(t *testing.T) {
+		record := "This is an spf record"
+		hasSPF := HasSPFRecord(record)
+
+		if !hasSPF {
+			t.Error("Expected a valid SPF record, but got a false result")
+		}
+	})
+
+	t.Run("Valid SPF Record (Contains '-all')", func(t *testing.T) {
+		record := "v=spf1 -all"
+		hasSPF := HasSPFRecord(record)
+
+		if !hasSPF {
+			t.Error("Expected a valid SPF record, but got a false result")
+		}
+	})
+
+	t.Run("Valid SPF Record (Contains '~all')", func(t *testing.T) {
+		record := "v=spf1 ~all"
+		hasSPF := HasSPFRecord(record)
+
+		if !hasSPF {
+			t.Error("Expected a valid SPF record, but got a false result")
+		}
+	})
+
+	t.Run("Non-SPF Record", func(t *testing.T) {
+		record := "This is a regular text record"
+		hasSPF := HasSPFRecord(record)
+
+		if hasSPF {
+			t.Error("Expected a non-SPF record, but got a true result")
+		}
+	})
 
 	// Add more test cases as needed.
 }
