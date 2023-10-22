2803de4c1ae4c3915d3b30d65d7de6c7b4395d59
tushargupta0694 <tushar.behror@gmail.com>
2023-10-22 09:12:57 +0530

    add test cases

--- dns_information/ns_lookup_test.go
+++ dns_information/ns_lookup_test.go
@@ -46,3 +46,46 @@ func TestGetTXTRecordNSLookup(t *testing.T) {
 
 	// Add more test cases as needed.
 }
+
+func TestGetDMARCRecordNSLookup(t *testing.T) {
+	t.Run("Valid DMARC Records", func(t *testing.T) {
+		// Mock the net.LookupTXT function to return DMARC records.
+		domain := "google.com"
+		dmarcRecords, err := GetDMARCRecordNSLookup(domain)
+
+		if err != nil {
+			t.Errorf("Expected no error, but got an error: %v", err)
+		}
+
+		expectedRecords := []string{
+			"DMARC Record 1",
+			"DMARC Record 2",
+		}
+
+		if len(dmarcRecords) <= 0 {
+			t.Errorf("Expected DMARC records %v, but got %v", expectedRecords, dmarcRecords)
+		}
+	})
+
+	t.Run("No DMARC Records", func(t *testing.T) {
+		// Mock the net.LookupTXT function to return an empty result.
+		domain := ".com"
+		dmarcRecords, _ := GetDMARCRecordNSLookup(domain)
+
+		if len(dmarcRecords) > 0 {
+			t.Error("Expected no DMARC records, but got some records")
+		}
+	})
+
+	t.Run("Non-Existent Domain", func(t *testing.T) {
+		// Mock the net.LookupTXT function to return an error for a non-existent domain.
+		domain := ".com"
+		_, err := GetDMARCRecordNSLookup(domain)
+
+		if err == nil {
+			t.Error("Expected an error for a non-existent domain, but got none")
+		}
+	})
+
+	// Add more test cases as needed.
+}
