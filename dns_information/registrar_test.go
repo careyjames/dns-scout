b020a1dd371e37d7f7da05924ae23dfcc17aa102
tushargupta0694 <tushar.behror@gmail.com>
2023-10-22 09:55:29 +0530

    add test cases

--- dns_information/registrar_test.go
+++ dns_information/registrar_test.go
@@ -1,7 +1,13 @@
 package dnsinformation
 
 import (
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
+
+	"github.com/careyjames/DNS-Scout/color"
+	constants "github.com/careyjames/DNS-Scout/constant"
 )
 
 func TestGetRegistrar(t *testing.T) {
@@ -47,4 +53,50 @@ func TestGetRegistrar(t *testing.T) {
 	})
 
 	// Add more test cases as needed.
+}
+
+func TestGetRegistrarPrompt(t *testing.T) {
+	// Redirect stdout for testing the output
+	originalStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	t.Run("Display Registrar Information", func(t *testing.T) {
+		input := "example.com"
+		GetRegistrarPromt(input, false) // Domain input
+		w.Close()
+		capturedOutput, _ := ioutil.ReadAll(r)
+
+		expectedOutput := color.Blue(" Registrar: ") + color.Green("Example Registrar") + constants.Newline
+		if strings.Contains(string(capturedOutput), expectedOutput) {
+			t.Errorf("Expected output displaying registrar information, but got: %s", string(capturedOutput))
+		}
+	})
+
+	t.Run("Display 'Unknown or Classified'", func(t *testing.T) {
+		input := "invalid.example"
+		GetRegistrarPromt(input, false) // Invalid domain input
+		w.Close()
+		capturedOutput, _ := ioutil.ReadAll(r)
+
+		expectedOutput := color.Blue(" Registrar: ") + color.Green("Unknown or ") + color.Yellow("Classified") + constants.Newline
+		if strings.Contains(string(capturedOutput), expectedOutput) {
+			t.Errorf("Expected output displaying 'Unknown or Classified', but got: %s", string(capturedOutput))
+		}
+	})
+
+	t.Run("Display Registrar Information for IP", func(t *testing.T) {
+		input := "192.168.1.1"
+		GetRegistrarPromt(input, true) // IP address input
+		w.Close()
+		capturedOutput, _ := ioutil.ReadAll(r)
+
+		expectedOutput := color.Blue(" Registrar: ") + color.Green("Registrar for IP addresses not available") + constants.Newline
+		if strings.Contains(string(capturedOutput), expectedOutput) {
+			t.Errorf("Expected output displaying registrar information for IP, but got: %s", string(capturedOutput))
+		}
+	})
+
+	// Restore the original stdout
+	os.Stdout = originalStdout
 }
