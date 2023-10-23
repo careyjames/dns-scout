464d2f7bf4c836bde465c0bd35fb7bcc03d901f0
Carey Balboa <carey.balboa@machelpnashville.com>
2023-10-23 14:16:07 -0500

    update generate_sitemap

---
+++ generate_sitemap/generate_sitemap.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+type Url struct {
+	Loc string `xml:"loc"`
+}
+
+type Urlset struct {
+	XMLName xml.Name `xml:"urlset"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Urls    []Url    `xml:"url"`
+}
+
+func GenerateSitemap() {
+	urls := []Url{
+		{Loc: "https://dns-scout.com/"},
+		{Loc: "https://dns-scout.com/about"},
+		// Add more URLs here
+	}
+
+	urlset := Urlset{
+		Xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
+		Urls:  urls,
+	}
+
+	output, err := xml.MarshalIndent(urlset, " ", "  ")
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	file, err := os.Create("sitemap.xml")
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	file.WriteString(xml.Header)
+	file.Write(output)
+}
