51d77cedec8878596f7f1ef5e3e0955d8d9b3069
Carey Balboa <carey.balboa@machelpnashville.com>
2023-10-29 19:42:26 -0500

    update

--- constant/constants.go
+++ constant/constants.go
@@ -3,7 +3,7 @@ package constants
 // IPInfoAPIURL is and API URL
 const (
 	IPInfoAPIURL        = "https://ipinfo.io/"
-	Version             = "6.7"
+	Version             = "6.8"
 	Newline             = "\n"
 	GooglePublicDNS     = "8.8.8.8:53"
 	CloudflarePublicDNS = "1.1.1.1:53"
