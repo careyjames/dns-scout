bfe5561c03c68bd4723d570fd6a11ee29f481696
Celliwig <celliwig@nym.hush.com>
2023-11-04 23:43:28 +0000

    Removed extra directory creation

--- build-launchpad.sh
+++ build-launchpad.sh
@@ -8,22 +8,12 @@ GPG_KEY_ID="EF536354988BF362947FC6FDBEB7932396E8FB23"
 GPG_KEY_ID="27B07EC1CEB62E5BBA0947F8D813438DD52A4F0C"
 # Define the project root directory
 project_root=`realpath $(pwd)`
-project_debsrc="${project_root}/../dns-scout_debsource/"
 export VERSION=$(grep Version constant/constants.go  | awk -F '=' '{print $2}'| awk -F'"' '{print $2}')
 
 # Run env subtitute
 envsubst < dns-scout.metainfo.xml.tpl > dns-scout.metainfo.xml
 #envsubst < debian/files.tpl > debian/files
 #envsubst < debian/changelog.tpl > debian/changelog
-
-# Create source directory
-mkdir -p "${project_debsrc}"
-cd "${project_debsrc}"
-
-# Create a symlink to project directory
-ln -s "${project_root}" dns-scout
-# And change to that
-cd dns-scout
 
 # Remove existing upstream tarball if it exists
 [ -f "../dns-scout_${VERSION}.orig.tar.gz" ] && rm "../dns-scout_${VERSION}.orig.tar.gz"
@@ -38,7 +28,7 @@ tar czvf "../dns-scout_${VERSION}.orig.tar.gz" --exclude='.git' --exclude='.gita
 						--exclude='*.tpl' \
 						--exclude='./bin/*' \
 						--exclude='./dns-scout-linux-*' \
-						-C "${project_debsrc}/dns-scout" .
+						-C "${project_root}/dns-scout" .
 
 # Download required GO modules
 go mod vendor
