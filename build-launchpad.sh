afb4ed7c4e5d03642278a7fa04ffe153b6ec4f2e
Carey Balboa <carey.balboa@machelpnashville.com>
2023-11-05 08:24:28 -0600

    update

--- build-launchpad.sh
+++ build-launchpad.sh
@@ -4,16 +4,14 @@
 # or update the path to the Go binary accordingly.
 # Define the GPG key ID
 GPG_KEY_ID="EF536354988BF362947FC6FDBEB7932396E8FB23"
-# Celliwig temp key
-GPG_KEY_ID="27B07EC1CEB62E5BBA0947F8D813438DD52A4F0C"
 # Define the project root directory
 project_root=`realpath $(pwd)`
 export VERSION=$(grep Version constant/constants.go  | awk -F '=' '{print $2}'| awk -F'"' '{print $2}')
 
 # Run env subtitute
 envsubst < dns-scout.metainfo.xml.tpl > dns-scout.metainfo.xml
-#envsubst < debian/files.tpl > debian/files
-#envsubst < debian/changelog.tpl > debian/changelog
+envsubst < debian/files.tpl > debian/files
+envsubst < debian/changelog.tpl > debian/changelog
 
 # Remove existing upstream tarball if it exists
 [ -f "../dns-scout_${VERSION}.orig.tar.gz" ] && rm "../dns-scout_${VERSION}.orig.tar.gz"
